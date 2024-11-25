0dc456486f1a10dc07d295fab5350dfe447dc943
Abigail :] <powheart26@proton.me>
2024-11-25 16:04:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder="api")
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://neondb_owner:QWzy9o8xUFCY@ep-long-brook-a51teh6g-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://neondb_owner:QWzy9o8xUFCY@ep-long-brook-a51teh6g-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"
 
 db = SQLAlchemy(app)
 
