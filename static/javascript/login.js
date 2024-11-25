afd5dd0d929f829376e9f04635c03ee0116cf094
Abigail :] <powheart26@proton.me>
2024-11-25 18:12:30 -0500

    Create login.js

---
+++ static/javascript/login.js
@@ -0,0 +1,66 @@
+window.onload = function(){
+  checkCookies();
+}
+
+function checkCookies() {
+	var found = false;
+  var value = null;
+ 	console.log("test");
+ 	var allCookies = document.cookie;
+ 	
+ 	var splitCookies = allCookies.split(";");
+ 	
+ 	for (var c = 0; c<=splitCookies.length; c++){
+ 		var cleanCookie;
+ 	    if (c != splitCookies.length){
+ 	      cleanCookie = splitCookies[c].trim();
+ 		}
+ 		var cleanerCookie = cleanCookie.split("=");
+ 		console.log(cleanerCookie);
+ 		if (cleanerCookie[c] === "register") {
+ 			console.log("register");
+			found = true;
+      valu = "register";
+ 		}
+    else if (cleanerCookie[c] === "login") {
+      console.log("login");
+      found = true;
+      value = "login";
+    }
+	 }
+	if (found == false){
+		storeCookies("register");
+    value = register;
+	}
+  return value;
+};
+
+function pageChange(){
+  var value = checkCookies();
+
+  if (value === "login"){
+    document.getElementById("login").style.visibility = "hidden";
+    document.getElementById("register").style.visibility = "visible";
+    register();
+  }
+  else if (value === "register"){
+    document.getElementById("login").style.visibility = "visible";
+    document.getElementById("register").style.visibility = "hidden";
+    login();
+  }
+}
+
+function storeCookies(name){
+  var myDate = new Date();
+	myDate.setMonth(myDate.getMonth() + 1);
+	document.cookie=name; expires="+myDate.toUTCString()+"; path=/";
+}
+
+function register() {
+	event.preventDefault();
+	storeCookies("register");
+}
+function login() {
+	event.preventDefault();
+	storeCookies("login");
+}
